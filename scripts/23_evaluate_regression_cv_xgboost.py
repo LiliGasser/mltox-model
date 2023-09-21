86562fd692382e5600e34b8d4431744e85beda9a
Lili <gasserli@ethz.ch>
2023-09-21 09:56:50 +0200

    WIP evaluate XGBoost CV

--- scripts/23_evaluate_regression_cv_xgboost.py
+++ scripts/23_evaluate_regression_cv_xgboost.py
@@ -28,12 +28,10 @@ path_figures = path_output + 'figures/'
 
 # %%
 
-# XGBoost: Fish data, both concentrations, 2023-08-14
+# XGBoost: Fish data, updated ADORE, 2023-09-15
 
-# completely new data pre-processing from ECOTOX 2022-09-15
-# for the four fingerprints
-# including chemical properties (mw, mp, ws, clogp)
-# groupsplit: totallyrandom, occurrence (no scaffolds)
+# data pre-processing from ECOTOX 2022-09-15
+# groupsplit: totallyrandom, occurrence
 
 #param_grid = [
     #{
@@ -61,7 +59,7 @@ path_figures = path_output + 'figures/'
     #}
 #]
 
-path_output_dir = path_vmoutput + '2023-08-14_bothconcentrations/'
+path_output_dir = path_vmoutput + '2023-09-15_from-updated-adore/'
 df_errors = utils.read_result_files(path_output_dir, file_type='error')
 
 # %%
@@ -105,14 +103,9 @@ df_oi = df_errors[df_errors['best_hp'] == True].copy()
 df_e_oi = df_errors[(df_errors['best_hp'] == True) &
                     (df_errors['set'] == 'valid') &
                     (df_errors['fold'] == 'mean')]
-df_e_oi
-
-# %%
 
 # mean errors (train and valid of 5-fold CV)
-df_e_v = df_oi[(df_oi['fold'] == 'mean')
-#               & (df_oi['set'] == 'valid')
-               ].copy()
+df_e_v = df_oi[(df_oi['fold'] == 'mean')].copy()
 
 list_cols = ['chem_fp', 'groupsplit', 'conctype', 'set', 'fold']
 list_cols += ['chem_prop', 'tax_pdm', 'tax_prop', 'exp']
@@ -165,7 +158,7 @@ for col_x in list_cols:
     list_tmp2 = []
 
     # for each fingerprint and groupsplit
-    for chem_fp in ['MACCS', 'pcp', 'Morgan', 'mol2vec']:
+    for chem_fp in ['MACCS', 'pcp', 'Morgan', 'ToxPrint']:   # TODO add mol2vec
         for groupsplit in ['totallyrandom', 'occurrence']:
 
             # get best hyperparams
