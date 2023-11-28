159bb22366b378ee46af4e758a45c7cd71b21183
Lili <gasserli@ethz.ch>
2023-11-28 11:34:41 +0100

    update 62 histogram

--- scripts/62_data_overview.py
+++ scripts/62_data_overview.py
@@ -52,13 +52,20 @@ df_long = pd.melt(df_eco,
                   value_vars=['result_conc1_mean_log', 'result_conc1_mean_mol_log'],
                   var_name='concentration_type',
                   value_name='concentration')
-df_long['concentration_type'] = df_long['concentration_type'].replace({'result_conc1_mean_log': 'mass concentration', 'result_conc1_mean_mol_log': 'molar concentration'})
+df_long['concentration_type'] = df_long['concentration_type'].replace({'result_conc1_mean_log': 'LC50 in $mg/L$', 'result_conc1_mean_mol_log': 'LC50 in $mol/L$'})
 
 g = (ggplot(data=df_long, mapping=aes(x='concentration'))
-    + geom_histogram(color='black', fill='black', bins=100)
+    + geom_histogram(color='#000', fill='#000', bins=100)
     + facet_wrap('~ concentration_type', ncol=1)
-    + theme_minimal()
-    + labs(x='log10-transformed concentration')
+    + theme_classic()
+    + theme(figure_size=(6,6))
+    + scale_x_continuous(breaks=[-10, -7.5, -5, -2.5, 0, 2.5, 5])
+    + theme(axis_text=element_text(size=12, color='black'))
+    + theme(axis_title=element_text(size=13, color='black'))
+    + labs(x='log10(LC50)')
+    + theme(strip_background=element_rect(fill='none', linetype='dashed'))
+    + theme(strip_text=element_text(size=12))
+    + theme(strip_align='left')
  )
 g.save(path_output + '62_concentration_histograms.pdf')
 g
